3d7a282f8aab3a12964fc65e74faf6183d58313a
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-03-08 01:43:56 +0530

    styled components :paintbrush:

--- capture/src/components/AboutSection.js
+++ capture/src/components/AboutSection.js
@@ -1,29 +1,66 @@
 import React from "react";
 import home1 from "../images/home1.png";
-const AboutSection = () => {
-  return (
-    <div>
-      <div className="description">
-        <div className="title">
-          <div className="hide">
-            <h2>We work to make</h2>
-          </div>
-          <div className="hide">
-            <h2>
-              your <span>dreams</span> come{" "}
-            </h2>
-          </div>
-          <div className="hide">
-            <h2>true.</h2>
-          </div>
-        </div>
-        <p>Contact us for photograpghy or videography and blah blah blah...</p>
-      </div>
-      <div className="image">
-        <img src={home1} alt="guy with a camera" />
-      </div>
-    </div>
-  );
-};
+import styled from 'styled-components';
 
-export default AboutSection;
+const AboutSection = () => {
+    return (
+      <About>
+        <Description>
+          <div className="title">
+            <Hide>
+              <h2>We work to make</h2>
+            </Hide>
+            <Hide>
+              <h2>
+                your <span>dreams</span> come
+              </h2>
+            </Hide>
+            <Hide>
+              <h2>true.</h2>
+            </Hide>
+          </div>
+          <p>
+            Contact us for any photography or videography ideas that you have. We
+            have professionals with amazing skills.
+          </p>
+          <button>Contact Us</button>
+        </Description>
+        <Image>
+          <img src={home1} alt="guy with a camera" />
+        </Image>
+      </About>
+    );
+  };
+  
+  //Styled Components
+  const About = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+  `;
+  const Description = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+    h2 {
+      font-weight: lighter;
+    }
+  `;
+  const Image = styled.div`
+    flex: 1;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 80vh;
+      object-fit: cover;
+    }
+  `;
+  
+  const Hide = styled.div`
+    overflow: hidden;
+  `;
+  
+  export default AboutSection;
+  
--- capture/src/components/App.js
+++ capture/src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import AboutUs from "../pages/Aboutus";
+import GlobalStyle from './globalstyle';
 
 const App = () => {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
