636e875fc41bb2585c48455a8d24982e7899bc66
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-03-11 19:47:46 +0530

    Added Animation :clapper:

--- capture/src/components/ServiceSection.js
+++ capture/src/components/ServiceSection.js
@@ -1,14 +1,21 @@
 import React from "react";
 import clock from "../images/clock.svg";
-import diaphragm from '../images/diaphragm.svg';
-import money from '../images/money.svg';
-import teamwork from '../images/teamwork.svg';
-import home2 from '../images/home2.png';
+import diaphragm from "../images/diaphragm.svg";
+import money from "../images/money.svg";
+import teamwork from "../images/teamwork.svg";
+import home2 from "../images/home2.png";
 
-import Styled from 'styled-components';
-import { About , Description , Image} from '../styles';
+import Styled from "styled-components";
+import { About, Description, Image } from "../styles";
+
+//TEST
+import { useInview } from "react-intersection-observer";
+import { useAnimation } from "framer-motion";
 
 const ServiceSection = () => {
+  const [element, view] = useView();
+  console.log(view);
+
   return (
     <Services>
       <Description>
@@ -18,36 +25,36 @@ const ServiceSection = () => {
         <Cards>
           <Card>
             <div className="icon">
-             <img src={clock} alt="clock" />
-             <h3>Efficient</h3> 
-             </div>
-             <p>Lorem ipsum dolor sit amet.</p>
+              <img src={clock} alt="clock" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
           </Card>
           <Card>
             <div className="icon">
-             <img src={teamwork} alt="clock" />
-             <h3>teamwork</h3> 
-             </div>
-             <p>Lorem ipsum dolor sit amet.</p>
+              <img src={teamwork} alt="clock" />
+              <h3>teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
           </Card>
           <Card>
             <div className="icon">
-             <img src={money} alt="clock" />
-             <h3>Affordable</h3> 
-             </div>
-             <p>Lorem ipsum dolor sit amet.</p>
+              <img src={money} alt="clock" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
           </Card>
           <Card>
             <div className="icon">
-             <img src={diaphragm} alt="clock" />
-             <h3>Diaphragm</h3> 
-             </div>
-             <p>Lorem ipsum dolor sit amet.</p>
+              <img src={diaphragm} alt="clock" />
+              <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
           </Card>
         </Cards>
       </Description>
       <Image>
-          <img src={home2} alt="home 2"/>
+        <img src={home2} alt="home 2" />
       </Image>
     </Services>
   );
@@ -66,7 +73,7 @@ p {
 const Cards = Styled.div`
     display:flex;
     flex-wrap: wrap;
-`
+`;
 const Card = Styled.div`
 flex-basis : 20rem;
 .icon {
@@ -80,6 +87,5 @@ flex-basis : 20rem;
   }
 }
 `;
-
 
 export default ServiceSection;
